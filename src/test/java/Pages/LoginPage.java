541e1dcbaedd3c447b2af969cd7e48e672d4247e
MAC <mac@MACs-Mac-mini.local>
2025-03-17 10:55:36 +0530

    Updated LoginPage and added LoginTest

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,8 +1,40 @@
 package Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
     WebDriver driver;
+
+    // constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // Locators
+    @FindBy(xpath = "//input[@name='email']")
+    WebElement email;
+
+    @FindBy(xpath = "//input[@name='password']")
+    WebElement password;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement login;
+
+    // Methods
+    public void enterUsername(String username) {
+        email.sendKeys(username);
+    }
+
+    public void enterPassword(String pwd) {
+        password.sendKeys(pwd);
+    }
+
+    public void clickLogin() {
+        login.click();
+    }
 
 }
---
+++ src/test/java/Tests/LoginTest.java
@@ -0,0 +1,38 @@
+package Tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Pages.LoginPage;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+        driver.get("https://echopro-dev.avlonline.com/login?goto=/contacts?current=1&pageSize=10");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    void testLogin() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterUsername("nisharg.shah@avlonline.com");
+        loginPage.enterPassword("nisharg");
+        loginPage.clickLogin();
+
+    }
+
+    @AfterClass
+    void tearDown() {
+        driver.quit();
+    }
+}
