93853a10013f6372c050c8a5d8ce6de75b5bdf91
MAC <mac@MACs-Mac-mini.local>
2025-03-21 14:43:44 +0530

    First

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,40 +1,35 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
     WebDriver driver;
 
-    // constructor
+    // Locators
+    By emailLocator = By.xpath("//input[@name='email']");
+    By passwordLocator = By.xpath("//input[@name='password']");
+    By loginButtonLocator = By.xpath("//button[@type='submit']");
+
+    // Constructor
     public LoginPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
     }
-
-    // Locators
-    @FindBy(xpath = "//input[@name='email']")
-    WebElement email;
-
-    @FindBy(xpath = "//input[@name='password']")
-    WebElement password;
-
-    @FindBy(xpath = "//button[@type='submit']")
-    WebElement login;
 
     // Methods
     public void enterUsername(String username) {
+        WebElement email = driver.findElement(emailLocator);
         email.sendKeys(username);
     }
 
     public void enterPassword(String pwd) {
+        WebElement password = driver.findElement(passwordLocator);
         password.sendKeys(pwd);
     }
 
     public void clickLogin() {
+        WebElement login = driver.findElement(loginButtonLocator);
         login.click();
     }
-
 }
